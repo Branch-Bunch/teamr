1ae20c1c1259e4b5daf768291e6854a48900b94f
Jacky Chiu <jacky.chiu888@gmail.com>
2017-01-21 18:13:42 -0500

    Moved loading of env var earlier

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-const dotenv = require('dotenv')
 const express = require('express')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+dotenv.config({ silent: true })
+
+const authRoutes = require('./routes/auth')
 
 const app = express()
-dotenv.config({ silent: true })
+mongoose.connect(process.env.MONGODB_URI)
+mongoose.Promise = Promise
 
 const PORT = process.env.PORT
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(`${__dirname}/public`))
+
+app.use('/', authRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
