a52e8a5ff9e8692a478804761b8bb7844ede304c
Jacky Chiu <jacky.chiu888@gmail.com>
2017-01-21 11:26:58 -0500

    Fixed undefined port

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express()
+dotenv.config({ silent: true })
+
 const PORT = process.env.PORT
 
-dotenv.config({ silent: true })
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(`${__dirname}/public`))
