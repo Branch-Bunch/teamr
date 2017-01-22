12fd2996d6699f643d114531f4bc6f3ae2cee9ef
Jacky Chiu <jacky.chiu888@gmail.com>
2017-01-22 04:32:46 -0500

    Removed comments

--- routes/users.js
+++ routes/users.js
@@ -3,30 +3,6 @@ const Users = require('../models/Users')
 
 const router = express.Router()
 
-/*
- * Used for testing purposes, no more push to production 2016
-const FakeUsers = [
-  {
-    "_id": "5883f32b70784004307bbe5e",
-    "name": "Jacky Chiu",
-    "bio": "node mentor",
-    "email": "idkhowtospell@gmail.com",
-    "location": "Ottawa, ON",
-    "imageUrl": "shiptoproduction.jpg",
-    "__v": 0
-  },
-  {
-    "_id": "asdhauifh3rh2389h32fh",
-    "name": "Vikram",
-    "bio": "node mentee",
-    "email": "idkhowtospelleaither@gmail.com",
-    "location": "Ottawa, ON",
-    "imageUrl": "testonproduction.jpg",
-    "__v": 0
-  },
-]
-*/
-
 router.get('/', (req, res) => {
   Users.find()
     .lean()
