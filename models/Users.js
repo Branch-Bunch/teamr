0a2986f6f0df7eb4c2ebea52e557fb3908832efc
Jacky Chiu <jacky.chiu888@gmail.com>
2017-01-21 20:59:28 -0500

    Added team section

--- models/Users.js
+++ models/Users.js
@@ -2,12 +2,18 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
+const userTeamSchema = new Schema({
+  id: { type: Schema.Types.ObjectId, required: true },
+  name: { type: String, required: true },
+})
+
 const usersSchema = new Schema({
   name: { type: String, required: true },
   bio: { type: String, required: true },
   email: { type: String, required: true },
   location: { type: String, required: true },
   imageUrl: { type: String },
+  team: { type: userTeamSchema },
 })
 
 const Users = mongoose.model('Users', usersSchema, 'Users')
