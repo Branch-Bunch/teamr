e4a23ba0374af225af4673202dcfe5b71030b698
vikrambombhi <vbombhi@gmail.com>
2017-01-21 23:22:53 -0500

    created endpoint for reciving data of individual users

--- routes/users.js
+++ routes/users.js
@@ -3,13 +3,8 @@ const Users = require('../models/Users')
 
 const router = express.Router()
 
-function getUsers() {
-  return new Promise((resolve, reject) => {
-    Users.find()
-    .then( users => console.log(users))
-  })
-}
-
+/*
+ * Used for testing purposes, no more push to production 2016
 const FakeUsers = [
   {
     "_id": "5883f32b70784004307bbe5e",
@@ -19,24 +14,49 @@ const FakeUsers = [
     "location": "Ottawa, ON",
     "imageUrl": "shiptoproduction.jpg",
     "__v": 0
-  }
+  },
+  {
+    "_id": "asdhauifh3rh2389h32fh",
+    "name": "Vikram",
+    "bio": "node mentee",
+    "email": "idkhowtospelleaither@gmail.com",
+    "location": "Ottawa, ON",
+    "imageUrl": "testonproduction.jpg",
+    "__v": 0
+  },
 ]
-
-
+*/
 
 router.get('/', (req, res) => {
-  //Users.find()
-  //  .then(users => res.send(users))
-  const normalized = FakeUsers.map((user) => {
-    return {
-      name: user.name,
-      location: user.location,
-      imageUrl: user.imageUrl,
-    }
-  })
-  res.send(normalized)
+  Users.find()
+    .lean()
+    .then((users) => {
+      const normalized = users.map((user) => {
+        return {
+          id: user._id,
+          name: user.name,
+          location: user.location,
+          imageUrl: user.imageUrl,
+        }
+      })
+      res.send(normalized)
+    })
+})
+
+router.get('/:id', (req, res) => {
+  Users.findById(req.params.id)
+    .lean()
+    .then((user) => {
+      res.send(user)
+    })
+    .catch((err) => {
+      res.status(500).send({
+        err: err.toString(),
+        givens: req.params,
+      })
+    })
 })
 
 module.exports = {
-  router
+  router,
 }
