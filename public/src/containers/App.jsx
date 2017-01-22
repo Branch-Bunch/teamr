aaf32cdd9a2aedecb323949c6057915581cd7901
Jacky Chiu <jacky.chiu888@gmail.com>
2017-01-22 02:38:17 -0500

    Added grid to app

--- public/src/containers/App.jsx
+++ public/src/containers/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Grid, Row, Col } from 'react-bootstrap'
+import CardContainer from './CardsContainer'
+import DetailedContainer from './DetailedContainer'
 
 const App = () => (
-  <h1>
-    Hello World
-  </h1>
+  <Grid>
+    <Row>
+      <Col>
+        <CardContainer />
+      </Col>
+      <Col>
+        <DetailedContainer />
+      </Col>
+    </Row>
+  </Grid>
 )
 
 export default connect(
