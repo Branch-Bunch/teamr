b1fdf02ff2c573ceed1f8ccf2f3e5ec3053f1291
vikrambombhi <vbombhi@gmail.com>
2017-01-22 05:47:33 -0500

    shipping to production to test, pray this works

--- public/src/components/CardList.jsx
+++ public/src/components/CardList.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Card from './Card'
 
-const CardList = ({ cards }) => (
-  cards.map((card, index) => (
+const CardList = ({ cards }) => {
+  const cardList = cards.map((card, index) => (
     <Card
       {...card}
       key={card._id}
     />
   ))
-)
+  return (
+    <div>
+      {cards}
+    </div>
+  )
+}
 
 export default CardList
--- public/src/containers/App.jsx
+++ public/src/containers/App.jsx
@@ -3,20 +3,16 @@ import { connect } from 'react-redux'
 import { Grid, Row, Col } from 'react-bootstrap'
 import CardContainer from './CardsContainer'
 import DetailedContainer from './DetailedContainer'
-import Card from '../components/Card'
 
 const App = () => (
     <div>
   <Grid>
     <Row>
-      <Col>
-        // <CardContainer />
-        // <Card />
-        <h1>asdsad</h1>
+      <Col md={7}>
+        <CardContainer />
       </Col>
-      <Col>
-        // <DetailedContainer />
-        <h1> asdsadasd </h1>
+      <Col md={5}>
+        <DetailedContainer />
       </Col>
     </Row>
   </Grid>
